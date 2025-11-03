b484b1cd1e124c43ca006a51f453bf6fac71096e
ZingerEngineer <ramy.yasser21102@gmail.com>
2025-11-03 22:39:48 +0200

    [Enhanced] Home Page:
    - Responsive design.
    - Simple.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,19 +2,67 @@
 // Migrated from: ramy-yasser-portfolio/src/app/[locale]/page.tsx
 // Home page component
 
+import { AppWindow, Phone } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
+import ramyYasser from '@/assets/images/ramy-formal.webp';
+import { CoolLink } from '@/components/customUi/CoolLink';
+import SectionCard from '@/components/customUi/SectionCard';
+import { useBreakpoint } from '@/hooks/useBreakPoint';
 
 export function Home() {
+	const breakpoint = useBreakpoint();
 	const { t } = useTranslation();
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center p-24">
-			<section className="">
-				<div>
-					<h1 className="font-black text-5xl">{t('Home.welcome')}</h1>
-					<p className="mt-4 text-lg">{t('Home.description')}</p>
+		<main className="flex min-h-screen flex-col items-center justify-center">
+			<SectionCard id={'image-section'} className="mt-0">
+				{breakpoint !== 'mobile' && (
+					<div className="flex flex-row items-center justify-center gap-8 w-full pt-10">
+						<img
+							src={ramyYasser}
+							alt={t('Home.imageAlt')}
+							className="min-w-1/5 max-w-1/3 aspect-auto"
+						/>
+						<div className="flex flex-col justify-center">
+							<h1 className="font-black lg:text-7xl md:text-5xl text-2xl">{t('Home.name')}</h1>
+							<p className="lg:mt-4 lg:text-2xl md:mt-2 md:text-lg mt-1 text-xs">
+								{t('Home.profession')}
+							</p>
+						</div>
+					</div>
+				)}
+				{breakpoint === 'mobile' && (
+					<div className="flex flex-col items-center justify-center gap-4 w-full">
+						<img
+							src={ramyYasser}
+							alt={t('Home.imageAlt')}
+							className="max-w-[70%] h-auto aspect-auto"
+						/>
+						<div className="flex flex-col justify-center">
+							<h1 className="font-black lg:text-7xl md:text-5xl text-2xl">{t('Home.name')}</h1>
+							<p className="lg:mt-4 lg:text-2xl md:mt-2 md:text-lg mt-1 text-xs">
+								{t('Home.profession')}
+							</p>
+						</div>
+					</div>
+				)}
+			</SectionCard>
+			<SectionCard id={'welcome-message-section'} className="my-0 justify-center">
+				<div className="flex flex-col items-center justify-center text-center">
+					<h1 className="font-black lg:text-5xl md:text-3xl text-sm">{t('Home.welcome')}</h1>
+					<p className="lg:text-lg md:text-sm text-xs mt-4">{t('Home.description')}</p>
+					<div className="mt-6 flex flex-row gap-4">
+						<CoolLink size="sm" variant="default" to="/projects">
+							<AppWindow className="size-5" />
+							{t('Home.projects')}
+						</CoolLink>
+						<CoolLink variant="default" to="/contact">
+							<Phone className="size-5" />
+							{t('Home.contact')}
+						</CoolLink>
+					</div>
 				</div>
-			</section>
+			</SectionCard>
 		</main>
 	);
 }
